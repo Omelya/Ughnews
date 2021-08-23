c634dee3d3aecf15b81ae2f2bce5d25c6238af7f
Omelyko <sekret.krut@gmail.com>
2021-08-23 13:33:23 +0300

    Add SpaController, Articles.vue, Home.vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,12 +19,24 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
 
-Vue.use(VueRouter)
+import VueRouter from 'vue-router';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import ElementUI from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(VueRouter);
+
+Vue.use(ElementUI);
+
+import App from './components/App'
+
+import Home from './components/Home'
+
+import Articles from './components/Articles'
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -32,6 +44,25 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+ const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/articles',
+            name: 'articles',
+            component: Articles
+        }
+    ]
+})
+
+
 const app = new Vue({
     el: '#app',
+    components: {App},
+    router
 });
---
+++ resources/js/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-container>
+        <el-header>
+            <el-menu :router="true" :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+                <el-menu-item  index="/">Home</el-menu-item>
+                <el-menu-item  index="articles">Articles</el-menu-item>
+            </el-menu>
+        </el-header>
+        <div>
+            <router-view></router-view>
+        </div>
+    </el-container> 
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                activeIndex: '/',
+            };
+        },
+        methods: {
+           
+        }
+    }
+</script>
